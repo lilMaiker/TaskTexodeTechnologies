d69a09f0428a41d569c30145d2eccf0c7475db4f
lilMaiker <fuhega45@gmail.com>
2022-02-27 00:29:23 +0300

    Вывод в таблицу с пользователями.

--- TrackerApplication/ViewModels/MainWindowViewModel.cs
+++ TrackerApplication/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using TrackerApplication.Data;
+using TrackerApplication.Infrastructure.Commands;
+using TrackerApplication.Models;
 using TrackerApplication.ViewModels.Base;
 
 namespace TrackerApplication.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        public ObservableCollection<PersonModel> People { get; set; }
+        private readonly DataAccessJson _da = new DataAccessJson();
         private string _Title = "TaskTexodeTechnologies";
 
         /// <summary>
@@ -16,6 +25,28 @@ namespace TrackerApplication.ViewModels
         {
             get => _Title;
             set => Set(ref _Title, value);
+        }
+
+        #region Commands
+
+        public ICommand MessageBoxCommand { get; }
+
+        private bool CanMessageBoxCommandExecuted(object p) => true;
+
+        private void OnMessageBoxCommandExecuted(object p)
+        {
+            MessageBox.Show("MsgBoxCommand");
+        }
+
+        #endregion
+
+       
+
+
+        public MainWindowViewModel()
+        {
+            People = new ObservableCollection<PersonModel>(_da.GetPersonsAndInfo());
+            MessageBoxCommand = new LambdaCommand(OnMessageBoxCommandExecuted, CanMessageBoxCommandExecuted);
         }
     }
 }
