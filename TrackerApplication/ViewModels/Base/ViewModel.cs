62393cd245a28d699982598e30dfd0a39e34edb2
lilMaiker <fuhega45@gmail.com>
2022-02-26 22:18:29 +0300

    Добавлен OnPropertyChanged.

---
+++ TrackerApplication/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TrackerApplication.Annotations;
+
+namespace TrackerApplication.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        //Distrucor
+        /*~ViewModel()
+        {
+            Dispose(false);
+        }*/
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if(!Disposing || _Disposed) return;;
+            _Disposed = true;
+        }
+    }
+}
+ 
