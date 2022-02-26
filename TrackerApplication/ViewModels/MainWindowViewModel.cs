2065a03b6ba329d01df7fc8ac49702dd9deeca63
lilMaiker <fuhega45@gmail.com>
2022-02-26 22:31:41 +0300

    Корректная работа View-Model.

--- TrackerApplication/ViewModels/MainWindowViewModel.cs
+++ TrackerApplication/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TrackerApplication.ViewModels.Base;
 
 namespace TrackerApplication.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        private string _Title = "TaskTexodeTechnologies";
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
     }
 }
